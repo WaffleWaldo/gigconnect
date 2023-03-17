6b124087ad5d754fd40c3da655a9a99e38067941
WaffleWaldo <oswaldoortiz0519@gmail.com>
2023-03-17 17:37:38 -0400

    added new route for users and more request methods

--- controllers/users.js
+++ controllers/users.js
@@ -2,4 +2,26 @@ const getAllUsers = (req, res) => {
     res.send("all users")
 }
 
-module.exports = { getAllUsers }
+const createUser = (req, res) => {
+    res.send("create user")
+}
+
+const getUser = (req, res) => {
+    res.send("single user")
+}
+
+const updateUser = (req, res) => {
+    res.send("update user info")
+}
+
+const deleteUser = (req, res) => {
+    res.send("delete user")
+}
+
+module.exports = { 
+    getAllUsers,
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser
+ }
