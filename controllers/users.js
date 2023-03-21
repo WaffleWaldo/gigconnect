6d2f64cd7a2671856a62398a62d1c0da07c195f4
WaffleWaldo <oswaldoortiz0519@gmail.com>
2023-03-21 14:49:08 -0400

    created a custom error and placed it in a controller funtion

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,26 @@
 const User = require("../models/User")
 const asyncWrapper = require("../middleware/async")
+const { createCustomError } = require("../errors/custom-error")
+
 
 const getAllUsers = asyncWrapper(async (req, res) => {
     const users = await User.find({}).select('-password')
     res.status(200).json({ users })
 })
 
-const createUser = async (req, res) => {
-    try {
+const createUser = asyncWrapper(async (req, res) => {
         const user = await User.create(req.body)
         res.status(201).json({ user })
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-}
+})
 
-const getUser = (req, res) => {
-    res.send("single user")
-}
+const getUser = asyncWrapper(async (req, res, next) => {
+    const { id: userID } = req.params
+    const user = await User.findOne({ _id: userID })
+    if (!user){
+        return next(createCustomError(`No user with id: ${userID}`, 404))
+    }
+    res.status(200).json({ user })
+})
 
 const updateUser = (req, res) => {
     res.send("update user info")
